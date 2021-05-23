65144601570810696b69825c94d80a995bc4938e
Nicicalu <caluori.nici@gmail.com>
2021-05-23 17:48:28 +0200

    123

--- app/main/controller/ApiController.py
+++ app/main/controller/ApiController.py
@@ -2,10 +2,10 @@ import re
 from flask import request, jsonify
 from flask_cors import CORS, logging, cross_origin
 from flask_restplus import Resource, Namespace, reqparse
+from app.main.model.GetModel import get
 
 api = Namespace('api', description='api operations')
 
-# if method == 'POST':
 
 @api.route('/')
 @api.doc('TEST OPERATION')
@@ -15,6 +15,16 @@ class DoUser(Resource):
     def post(self):
         return "Test successful"
 
+@api.route('/get')
+@api.doc('Route for logging in')
+@api.response(200, 'Login auth as json')
+class handleQuestion(Resource):
+    
+    def post(self):
+        table = request.json["table"]
+        result = get(table)
+        result = jsonify(result)
+        return result
 
 @api.route('/login')
 @api.doc('Route for logging in')
---
+++ app/main/model/GetModel.py
@@ -0,0 +1,4 @@
+from app.main.model.DatabaseModel import databaseQuery
+
+def get(table):
+    return table
