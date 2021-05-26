b5930e4ac9aa1102856e9872b1097f8b72dea376
Nicicalu <caluori.nici@gmail.com>
2021-05-26 09:36:13 +0200

    DELETE GET UND ADD funktionieren

--- app/main/model/DelModel.py
+++ app/main/model/DelModel.py
@@ -1,4 +1,13 @@
 from app.main.model.DatabaseModel import databaseQuery
 
-def del(table):
-    return table
+# Funktion für das Löschen von Daten
+def delete(data, table):
+    column = list(data.keys())[0] # erste Spalte
+    value = list(data.values())[0] # erster Wert
+    sqlQuery = "DELETE FROM tbl_" + table + " WHERE "+str(column)+" = '"+str(value)+"'" #Delete Query zusammenbauen
+    try:
+        data = databaseQuery(sqlQuery) #Query ausführen
+    except:
+        print("--------- FEHLER BEI DELETE QUERY ---------")
+        data = "Fehler. Query '"+sqlQuery+"' konnte nicht ausgeführt werden."
+    return data
--- app/main/model/SetModel.py
+++ app/main/model/SetModel.py
@@ -1,12 +1,14 @@
 from app.main.model.DatabaseModel import databaseQuery
 from app.main.service.HelperService import generate_insert_query, generate_update_query
 
+# Funktion für das Hinzufügen von Daten
 def add(data, table):
-    query = generate_insert_query(data, table)
-    result = databaseQuery(query)
+    query = generate_insert_query(data, table) # Query generieren lassen
+    result = databaseQuery(query) #Query ausführen
     return result
 
+# Funktion für das Ändern von Daten
 def update(data, table):
-    query = generate_update_query(data, table)
-    result = databaseQuery(query)
+    query = generate_update_query(data, table) # Query generieren lassen
+    result = databaseQuery(query) #Query ausführen
     return result
