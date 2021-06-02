fe6c435ed8d4a47cee1b252e89e3ae3f9573c68d
Nicicalu <caluori.nici@gmail.com>
2021-06-02 09:06:28 +0200

    App.run anstatt Manager

--- Manage.py
+++ Manage.py
@@ -29,5 +29,5 @@ def test():
     return 1
 
 if __name__ == '__main__':
-    manager.run() # Starte den Manager
-    #app.run(debug=False)
+    #manager.run() # Starte den Manager
+    app.run(debug=False, host="0.0.0.0")
